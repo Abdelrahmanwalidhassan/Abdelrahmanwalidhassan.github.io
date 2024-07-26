678e5f80eca5968847b37427b6788edf00849d17
Abd EL-Rahman Walid Hassan <97101497+Abdelrahmanwalidhassan@users.noreply.github.com>
2024-07-26 21:38:41 +0300

    first commit

---
+++ s.sh
@@ -0,0 +1,7 @@
+echo "Hello World" > index.html
+git init
+git add --all
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Abdelrahmanwalidhassan/Abdelrahmanwalidhassan.github.io.git
+git push -u origin main
