7f46e0f43714cf8a196ace9ff842dc8003cb1eca
Abd EL-Rahman Walid Hassan <97101497+Abdelrahmanwalidhassan@users.noreply.github.com>
2024-07-27 01:15:36 +0300

    commiting changes

---
+++ c/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const apiKey = process.env.REACT_APP_API_KEY;
+	console.log("API Key:", apiKey);
+	// Use the apiKey in your fetch requests or other logic
+});
