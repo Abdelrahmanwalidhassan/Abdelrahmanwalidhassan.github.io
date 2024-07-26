7076df0f19da9c6c2bbb99d28ed98f927048382a
Abd EL-Rahman Walid Hassan <97101497+Abdelrahmanwalidhassan@users.noreply.github.com>
2024-07-26 22:14:32 +0300

    commiting changes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 console.log(document.location.pathname);
 
-window.addEventListener("DOMContentLoaded", () => {
-	switch (document.location.pathname) {
-		case "/":
-			break;
-		case "/gg":
-			document.body.innerHTML = "gg";
-		default:
-			document.body.innerHTML = "404";
-			break;
-	}
-});
+// window.addEventListener("DOMContentLoaded", () => {
+switch (document.location.pathname) {
+	case "/":
+		break;
+	case "/gg":
+		document.body.innerHTML = "gg";
+	default:
+		document.body.innerHTML = "404";
+		break;
+}
+// });
